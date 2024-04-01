2b464b9c752a83f733da824f8d37629abf90ff45
ega471 <jorgelorien@gmail.com>
2024-04-01 21:04:24 +0200

    Indicador turno

--- lib/pages/game_screen.dart
+++ lib/pages/game_screen.dart
@@ -30,10 +30,6 @@ class _MapScreenState extends State<MapScreen> {
   double _positionChat = -300.0;
   final List<types.Message> _messages = [];
 
-  void _onTapDown(BuildContext context, TapDownDetails details) {
-    print('Usuario tocó en las coordenadas: ${details.globalPosition}');
-  }
-
   @override
   void initState() {
     super.initState();
@@ -65,10 +61,8 @@ class _MapScreenState extends State<MapScreen> {
                 FocusScope.of(context).unfocus();
               });
             },
-            child: Expanded(
-              child: MapWidget(
-                size: 400,
-              ),
+            child: const Expanded(
+              child: MapWidget(),
             ),
           ),
         ),
@@ -84,6 +78,7 @@ class _MapScreenState extends State<MapScreen> {
             child: const Icon(Icons.chat),
           ),
         ),
+        const Align(alignment: Alignment.bottomCenter, child: TurnInfo()),
         AnimatedPositioned(
           duration: const Duration(milliseconds: 250),
           top: 0,
@@ -110,6 +105,108 @@ class _MapScreenState extends State<MapScreen> {
                 backgroundColor: Color.fromARGB(255, 255, 206, 120),
               ),
               showUserNames: true,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TurnInfo extends StatelessWidget {
+  const TurnInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(20),
+          width: 350.0,
+          height: 60.0,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(175, 57, 57, 57),
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Nombre de usuario',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Divider(
+                height: 0,
+              ),
+              Text(
+                'INVERTIR',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 0,
+          top: 10,
+          child: Container(
+            width: 75.0,
+            height: 75.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blue,
+              border: Border.all(color: Colors.black, width: 3.0),
+            ),
+            child: const Icon(
+              Icons.supervised_user_circle,
+              size: 60,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          top: 10,
+          child: InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(37.5),
+            child: Container(
+              width: 75.0,
+              height: 75.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+                border: Border.all(color: Colors.black, width: 3.0),
+              ),
+              child: const Icon(
+                Icons.skip_next,
+                size: 60,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 17.5,
+          top: 70,
+          child: Container(
+            width: 40,
+            height: 25,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(25.0),
+              border: Border.all(color: Colors.black, width: 2.0),
+            ),
+            child: const Center(
+              child: Text(
+                '60',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ),
         ),

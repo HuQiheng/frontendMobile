dfb1f7de24781d521665f7b6c422efd6530b82a5
JJv03 <juanvalle2003@gmail.com>
2024-04-12 23:12:07 +0200

    Corrección de pantalla de amigos

--- lib/pages/HomePage/friends_screen.dart
+++ lib/pages/HomePage/friends_screen.dart
@@ -17,56 +17,66 @@ class FriendsScreen extends StatelessWidget {
         ),
         backgroundColor: const Color(0xFF083344),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Añadir amigo (Introducir código):',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: const Color(0xFF005A88),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
+      body: Container(
+        color: const Color(0xFF083344), // Color de fondo deseado
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Añadir amigo (Introducir código):',
+                        hintStyle: const TextStyle(color: Colors.white70),
+                        filled: true,
+                        fillColor: const Color(0xFF005A88),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
+                      style: const TextStyle(color: Colors.white),
                     ),
-                    style: const TextStyle(color: Colors.white),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.check, color: Colors.white),
+                    //Se busca en la base de datos
+                    onPressed: () {},
+                  ),
+                ],),
+                const SizedBox(height: 16), // Espacio entre el TextField y la lista
+                Container(
+                  height: 1,
+                  color: Colors.white24,
+                ),
+                const SizedBox(height: 16), // Espacio entre la línea y la lista
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: 10, // Cambia esto por la longitud de tu lista de amigos
+                    separatorBuilder: (context, index) =>
+                      const Divider(color: Colors.white24),
+                    itemBuilder: (BuildContext context, int index) {
+                      // Aquí puedes construir cada elemento de la lista
+                      return Material(
+                        color: const Color(0xFF0066CC), // Color de fondo del ListTile
+                        child: ListTile(
+                          title: Text(
+                            'Amigo $index',
+                            style: TextStyle(color: Colors.white), // Color del texto del ListTile
+                          ),
+                          onTap: () {
+                            // Acción al hacer tap en el amigo
+                          },
+                        ),
+                      );
+                    },
                   ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.check, color: Colors.white),
-                  //Se busca en la base de datos
-                  onPressed: () {},
-                ),
-              ],),
-              const SizedBox(height: 16), // Espacio entre el TextField y la lista
-              Container(
-                height: 1,
-                color: Colors.white24,
-              ),
-              const SizedBox(height: 16), // Espacio entre la línea y la lista
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 10, // Cambia esto por la longitud de tu lista de amigos
-                  itemBuilder: (BuildContext context, int index) {
-                    // Aquí puedes construir cada elemento de la lista
-                    return ListTile(
-                      title: Text('Amigo $index'),
-                      tileColor: const Color(0xFF0066CC),
-                      onTap: () {
-                        // Acción al hacer tap en el amigo
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

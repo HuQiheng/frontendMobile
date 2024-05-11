cead77c8ac3e739b05380d0f47eb715a93daad37
844759 <angel.viag2003@gmail.com>
2024-05-11 19:31:19 +0200

    TurnManagement

--- lib/methods/player_class.dart
+++ lib/methods/player_class.dart
@@ -2,11 +2,14 @@ class Player {
   final String email;
   final String profileImageUrl;
   final String name;
+  bool surrender;
 
-  Player(
-      {required this.email,
-      this.profileImageUrl = '/profile.svg',
-      required this.name});
+  Player({
+    required this.email,
+    this.profileImageUrl = '/profile.svg',
+    required this.name,
+    this.surrender = false,
+  });
 
   factory Player.fromEmailNamePicture(
       String email, String name, String picture) {
